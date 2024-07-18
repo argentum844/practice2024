ed361ea4e0b15976e68aff83cc06f76c0ea734ea
Егор Осинкин <osinkineg@yandex.ru>
2024-07-18 17:18:11 +0300

    Создал начальный проект

---
+++ MainProject/src/main/java/org/example/CalculatorAlgorithm.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class CalculatorAlgorithm {
+    public int plus(int n1, int n2) {
+        return n1 + n2;
+    }
+    public int minus(int n1, int n2){
+        return n1-n2;
+    }
+    public int mul(int n1, int n2){
+        return n1*n2;
+    }
+    public int div(int n1, int n2){
+        return n1/n2;
+    }
+}
